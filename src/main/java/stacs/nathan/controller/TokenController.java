f851673e96c6dd87477d90a021f1df052ace496b
Tracy Thanda Aye <tracy.thandaaye@gmail.com>
2020-05-06 19:13:36 +0800

    #PILOT-53 #testing codepipeline2

--- src/main/java/stacs/nathan/controller/TokenController.java
+++ src/main/java/stacs/nathan/controller/TokenController.java
@@ -1,4 +1,21 @@
 package stacs.nathan.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class TokenController {
+
+    @CrossOrigin
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return  new ResponseEntity<>("Nathan health check has been successful.", HttpStatus.OK);
+    }
+
+
 }
