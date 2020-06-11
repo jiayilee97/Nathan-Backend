6be11708f82eb00a36873cfae9c172e80c8f7da7
aaron <aaronsohyh95@gmail.com>
2020-06-11 11:10:11 +0800

    Removing broken query

--- src/main/java/stacs/nathan/repository/SPTokenRepository.java
+++ src/main/java/stacs/nathan/repository/SPTokenRepository.java
@@ -14,12 +14,12 @@ import java.util.List;
 public interface SPTokenRepository extends JpaRepository<SPToken, Long> {
 
 
-//  @Query("SELECT sp FROM SPToken sp WHERE sp.user = :user AND sp.status = :status")
-//  List<SPTokenResponseDto> fetchAllOpenPositions(@Param("user") User user, @Param("status") SPTokenStatus status);
-
-  @Query("SELECT NEW stacs.nathan.dto.response.SPTokenResponseDto(sp.tokenCode, sp.productType, sp.contractInceptionDate, sp.underlyingCurrency, sp.notionalAmount, sp.fixingAmount, sp.spotPrice, sp.strikeRate, sp.knockOutPrice, sp.maturityDate, sp.fixingPage, sp.numberOfFixing, sp.cpId, sp.opsId, sp.issuingAddress)" +
-          "FROM SPToken sp WHERE sp.user = :user AND sp.status = :status")
+  @Query("SELECT sp FROM SPToken sp WHERE sp.user = :user AND sp.status = :status")
   List<SPTokenResponseDto> fetchAllOpenPositions(@Param("user") User user, @Param("status") SPTokenStatus status);
+
+//  @Query("SELECT NEW stacs.nathan.dto.response.SPTokenResponseDto(sp.tokenCode, sp.productType, sp.contractInceptionDate, sp.underlyingCurrency, sp.notionalAmount, sp.fixingAmount, sp.spotPrice, sp.strikeRate, sp.knockOutPrice, sp.maturityDate, sp.fixingPage, sp.numberOfFixing, sp.cpId, sp.opsId, sp.issuingAddress)" +
+//          "FROM SPToken sp WHERE sp.user = :user AND sp.status = :status")
+//  List<SPTokenResponseDto> fetchAllOpenPositions(@Param("user") User user, @Param("status") SPTokenStatus status);
 
   @Query("SELECT sp FROM SPToken sp WHERE sp.user = :user AND sp.status != :status")
   List<SPTokenResponseDto> fetchAllClosedPositions(@Param("user") User user, @Param("status") SPTokenStatus status);
