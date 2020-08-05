d7ec93f3a1ef2f2d33852c2d0a8c6e930341d1d7
Tracy Thanda Aye <tracy.thandaaye@gmail.com>
2020-08-05 15:58:44 +0800

    modified CommonUtils

--- src/main/java/stacs/nathan/service/TransactionHistoryServiceImpl.java
+++ src/main/java/stacs/nathan/service/TransactionHistoryServiceImpl.java
@@ -1,25 +1,24 @@
 package stacs.nathan.service;
 
-import org.apache.commons.lang3.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import stacs.nathan.core.exception.ServerErrorException;
 import stacs.nathan.dto.response.TransactionHistoryResponseDto;
 import stacs.nathan.entity.TransactionHistory;
 import stacs.nathan.repository.TransactionRepository;
+import stacs.nathan.utils.CommonUtils;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class TransactionHistoryServiceImpl implements TransactionHistoryService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionHistoryServiceImpl.class);
 
     @Autowired
     TransactionRepository repository;
-
-    @Autowired
-    UserService userService;
 
     public void save(TransactionHistory transactionHistory){
         repository.save(transactionHistory);
@@ -27,9 +26,15 @@ public class TransactionHistoryServiceImpl implements TransactionHistoryService 
 
     @Override
     public List<TransactionHistoryResponseDto> fetchAllTransactionHistory(String startDate, String endDate) throws ServerErrorException, ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        Date start = DateUtils.addDays(formatter.parse(startDate), -1);
-        Date end = DateUtils.addDays(formatter.parse(endDate), 1);
-        return repository.findAllByCreatedBy(start, end);
+        LOGGER.debug("Entering fetchAllTransactionHistory().");
+        try {
+            Date start = CommonUtils.formatDate(startDate, -1);
+            Date end = CommonUtils.formatDate(endDate, 1);
+            return repository.findAllByCreatedBy(start, end);
+        } catch (Exception e){
+            LOGGER.error("Exception in fetchAllTransactionHistory().", e);
+            throw new ServerErrorException("Exception in fetchAllTransactionHistory().", e);
+        }
+
     }
 }
