95bb40a3ab15d787407a09d93620e7c55e5ecacd
aaron <aaronsohyh95@gmail.com>
2020-08-21 14:23:10 +0800

    PILOT-322 Nathan Audit Log

--- src/main/java/stacs/nathan/core/audit/action/service/ActionAuditTrailService.java
+++ src/main/java/stacs/nathan/core/audit/action/service/ActionAuditTrailService.java
@@ -2,6 +2,8 @@ package stacs.nathan.core.audit.action.service;
 
 import stacs.nathan.core.audit.action.entity.ActionAuditTrailEntity;
 import stacs.nathan.core.exception.ServerErrorException;
+import stacs.nathan.dto.response.AuditLogResponseDto;
+
 import java.util.List;
 
 public interface ActionAuditTrailService {
