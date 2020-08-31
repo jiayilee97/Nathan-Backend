37f397e155c25edad93aae3c336af126b8481593
aaron <aaronsohyh95@gmail.com>
2020-08-31 17:04:47 +0800

    PILOT-371 Create Credit Risk User role

--- src/main/java/stacs/nathan/controller/AuditController.java
+++ src/main/java/stacs/nathan/controller/AuditController.java
@@ -18,7 +18,7 @@ public class AuditController {
     @Autowired
     private ActionAuditTrailService actionAuditTrailService;
 
-    @PreAuthorize("hasAuthority('OPS')")
+    @PreAuthorize("hasAuthority('OPS') or hasAuthority('RISK')")
     @GetMapping("/fetch-all")
     public List<ActionAuditTrailEntity> fetchAllAuditActions() throws ServerErrorException { return actionAuditTrailService.findAll(); }
 }
